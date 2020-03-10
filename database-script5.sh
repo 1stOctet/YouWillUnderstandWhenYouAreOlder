17f3c6a97aeaca4240a9b08b3125acc023d4fb2c
1stOctet <59316556+1stOctet@users.noreply.github.com>
2020-03-09 22:50:05 -0400

    database-script5.sh

---
+++ database-script5.sh
@@ -0,0 +1,19 @@
+
+#backup sqldb
+echo "backing up /etc/pihole/gravity.db to /etc/pihole/gravity.$(date +%F_%R).db"
+cp /etc/pihole/gravity.db /etc/pihole/gravity.$(date +%F_%R).db
+
+echo "link domains to group 1"
+sqlite3 /etc/pihole/gravity.db <<EOF
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (1, 1);
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (2, 1);
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (3, 1);
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (4, 1);
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (5, 1);
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (6, 1);
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (7, 1);
+INSERT INTO domainlist_by_group (domainlist_id, group_id) VALUES (8, 1);
+EOF
+
+echo "Done"
+# https://github.com/pi-hole/docs/blob/release/v5.0/docs/database/gravity/example.md
